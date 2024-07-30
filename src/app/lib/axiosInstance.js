24004a0e070801909686bab37466481df14f4498
Tarun <tarun.mahajan@elxoinc.com>
2024-07-30 16:38:36 +0530

    new

--- src/app/lib/axiosInstance.js
+++ src/app/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"https://food-delivery-eight-mu.vercel.app/"
+    baseURL:"http://localhost:3000/api/"
 });
 
 const header={

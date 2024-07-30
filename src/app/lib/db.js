c74550eeaf85a68a75de8ec63288099c6806e49d
Tarun <tarun.mahajan@elxoinc.com>
2024-07-30 13:23:45 +0530

    change

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,3 +1,4 @@
 const {username,password} = process.env
 
+// export const connectionStr = `mongodb+srv://${username}:${password}@next-messaging.ummorjo.mongodb.net/restoDB?retryWrites=true&w=majority&appName=next-messaging`
 export const connectionStr = `mongodb+srv://${username}:${password}@next-messaging.ummorjo.mongodb.net/restoDB?retryWrites=true&w=majority&appName=next-messaging`

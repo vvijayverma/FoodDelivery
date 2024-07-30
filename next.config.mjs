09ad45ac51e62db3468e4b5d02fd1bc0c2b36a64
Tarun <tarun.mahajan@elxoinc.com>
2024-07-30 12:07:00 +0530

    food delivey

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,69 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: [
+//           'www.shutterstock.com',
+//           'cdn.pixabay.com',
+//           'static.toiimg.com',
+//           'unsplash.com',
+//           'images.unsplash.com',
+//           'png.pngtree.com',
+//           'encrypted-tbn0.gstatic.com'
+//         ],
+//       },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          'www.shutterstock.com',
-          'cdn.pixabay.com',
-          'static.toiimg.com',
-          'unsplash.com',
-          'images.unsplash.com',
-          'png.pngtree.com',
-          'encrypted-tbn0.gstatic.com'
-        ],
-      },
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'www.shutterstock.com',
+              port: '',
+              pathname: '/**',
+          },
+          {
+              protocol: 'https',
+              hostname: 'cdn.pixabay.com',
+              port: '',
+              pathname: '/**',
+          },
+          {
+              protocol: 'https',
+              hostname: 'static.toiimg.com',
+              port: '',
+              pathname: '/**',
+          },
+          {
+              protocol: 'https',
+              hostname: 'unsplash.com',
+              port: '',
+              pathname: '/**',
+          },
+          {
+              protocol: 'https',
+              hostname: 'images.unsplash.com',
+              port: '',
+              pathname: '/**',
+          },
+          {
+              protocol: 'https',
+              hostname: 'png.pngtree.com',
+              port: '',
+              pathname: '/**',
+          },
+          {
+              protocol: 'https',
+              hostname: 'encrypted-tbn0.gstatic.com',
+              port: '',
+              pathname: '/**',
+          },
+      ],
+  },
 };
 
 export default nextConfig;
+
